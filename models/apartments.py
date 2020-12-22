005452dd51f17e2c2f06611d9a65acefd219c92b
axl-codes <axl.lanca@gmail.com>
2020-12-23 00:20:31 +0100

    import schema Table

--- models/apartments.py
+++ models/apartments.py
@@ -1,6 +1,13 @@
-from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from db_management.base import Base
+from sqlalchemy.schema import Table
+
+association_table = Table(
+    'apartments_users', Base.metadata,
+    Column('apartment_id', Integer, ForeignKey('apartments.id')),
+    Column('user_id', Integer, ForeignKey('users.id'))
+)
 
 class Apartment(Base):
     __tablename__ = "apartments"
@@ -10,4 +17,4 @@ class Apartment(Base):
     door_number = Column(Integer)
     typology = Column(String)
     area = Column(Float)
-
+    users = relationship("User", secondary=association_table, backref="apartments")
